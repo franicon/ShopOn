1fe7cb8681cd8f73739e4726311bffd5858f4a2c
Abayomi Aluko <franicons@gmail.com>
2023-07-29 14:44:15 +0100

    fix: setting-up-mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
+import { Config } from './config';
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { ConfigSchema } from './config/shcema';
 
 @Module({
-  imports: [],
+  imports: [
+    //  Config
+    ConfigModule.forRoot({
+      load:[Config],
+      isGlobal: true,
+      expandVariables: true,
+      validationSchema: ConfigSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
